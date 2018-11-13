b28bf5ec152a0cc44e45e4121832a7905c2973dd
SparksFullStack <csparks@antioch.edu>
2018-11-13 11:55:53 -0800

    mongo wireframe setup complete

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport'); // this type of import runs the file but doesn't allow you to use it in the current file
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// configuring the database
+mongoose.Promise = global.Promise;
+const databaseOptions = {
+    useNewUrlParser: true,
+}
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(process.env.MONGO_URL, databaseOptions);
+mongoose.connection
+    .once('open', () => console.log(`The database is connected!`))
+    .on('error', (err) => console.log(`There was an error starting the database: \n${err}`));
 
 // routers
 require('./routes/authRoutes')(app);
