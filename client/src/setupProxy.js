2625c78f948ffdf86a2ccd2f7d621c34f8ef1b01
SparksFullStack <csparks@antioch.edu>
2018-12-12 15:02:43 -0800

    first action creator for auth status completed

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,7 @@
 const proxy = require('http-proxy-middleware')
  
 module.exports = function(app) {
+    // values provided here are what forward along any Axios requests to the target URL provided
     app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
     app.use(proxy('/api/*', { target: 'http://localhost:5000' }));
 }
