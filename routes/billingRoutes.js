9e44da1fb92edea46909dc366096e867a956423e
SparksFullStack <csparks@antioch.edu>
2018-12-30 12:07:57 -0800

    charge fully working--credit setup is next

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,7 +1,14 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = (app) => {
-    app.post('/api/stripe', (req, res) => {
-        console.log(req.body);
+    app.post('/api/stripe', async (req, res) => {
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '$5 for 5 credits',
+            source: req.body.id
+        });
+
+        console.log(charge);
     });
 }
