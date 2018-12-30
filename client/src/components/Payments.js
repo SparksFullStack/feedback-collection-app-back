853b1f9b69c79394483edc1675541cd1024afdf1
SparksFullStack <csparks@antioch.edu>
2018-12-30 11:23:36 -0800

    updated payments component to be more eye-catching

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,10 +6,14 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout 
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         token={ (token) => console.log(token) }
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button class="btn">Add Credits</button>
+      </StripeCheckout>
     )
   }
 }
