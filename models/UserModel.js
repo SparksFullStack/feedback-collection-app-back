9d5f1bc5a2b599505223b3a518fb5453b3c53ee9
SparksFullStack <csparks@antioch.edu>
2018-12-30 12:14:57 -0800

    stripe route handler finished and sends back the user model post-charge

--- models/UserModel.js
+++ models/UserModel.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    credits: {
+        type: Number,
+        default: 0,
+    }
 })
 
 mongoose.model('users', userSchema);
